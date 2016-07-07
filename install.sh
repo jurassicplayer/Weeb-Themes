e508b4dbe0d7b38d92cb780677c306a5d568e678
jurassicplayer <jurassicplayer75@gmail.com>
2016-07-07 03:56:10 -0700

    Separated QT4 and QT5 versions. Minor clean-up. Improved some shoddy scaling.

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cp -rf weeb-sddm-themes/* /usr/share/sddm/themes/
-cp -rf weeb-gtk-themes/* /usr/share/themes/
+plasmapkg2 -t lookandfeel -i weeb-ksplashx-themes/SnowyNightMiku_QT5
+sudo cp -rf weeb-sddm-themes/* /usr/share/sddm/themes/
+sudo cp -rf weeb-gtk-themes/* /usr/share/themes/
+sudo cp -rf weeb-ksplashx-themes/SnowyNightMiku_QT4 /usr/share/ksplash/Themes
