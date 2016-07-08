d4d727be3ef3a566bfb7ebc6ab97560c37b91cf9
jurassicplayer <jurassicplayer75@gmail.com>
2016-07-07 17:30:46 -0700

    Made QT5 KSplash attempt removing the previous before installing.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-plasmapkg2 -t lookandfeel -i weeb-ksplashx-themes/SnowyNightMiku_QT5
+plasmapkg2 -t lookandfeel -r SnowyNightMiku_QT5; plasmapkg2 -t lookandfeel -i weeb-ksplashx-themes/SnowyNightMiku_QT5
 sudo cp -rf weeb-sddm-themes/* /usr/share/sddm/themes/
 sudo cp -rf weeb-gtk-themes/* /usr/share/themes/
 sudo cp -rf weeb-ksplashx-themes/SnowyNightMiku_QT4 /usr/share/ksplash/Themes
